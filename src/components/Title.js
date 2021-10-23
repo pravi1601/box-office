381e7610e84b431e7f4d372b7bc5893c9306965c
pravi1601 <ponnapallipravallika2@gmail.com>
2021-10-23 19:10:32 +0530

    final

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React, { memo } from 'react';
+import { TitleWrapper } from './Titled.styled';
+
+const Title = ({ title, subtitle }) => {
+  return (
+    <TitleWrapper>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </TitleWrapper>
+  );
+};
+
+export default memo(Title);
